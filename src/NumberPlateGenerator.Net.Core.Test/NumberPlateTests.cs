a75b27a45bfd33075bc78f161ec316837337a3cd
Jan <64534642+st0o0@users.noreply.github.com>
2023-09-18 20:31:54 +0000

    add more fixes

--- src/NumberPlateGenerator.Net.Core.Test/NumberPlateTests.cs
+++ src/NumberPlateGenerator.Net.Core.Test/NumberPlateTests.cs
@@ -4,12 +4,6 @@ namespace NumberPlateGenerator.Net.Core.Test
 {
     public class NameplateTests
     {
-<<<<<<< HEAD
-        var result = NumberPlate.GetGenerator(NumberPlateType.Unknown);
-        Assert.NotNull(result);
-        Assert.IsType<TestGenerator>(result);
-        Assert.Equal(TestGenerator.Type, result.NumberPlateType);
-=======
         [Fact]
         public void NumberPlateTest()
         {
@@ -18,6 +12,5 @@ namespace NumberPlateGenerator.Net.Core.Test
             Assert.IsType<TestGenerator>(result);
             Assert.Equal(NumberPlateType.Unknown, result.Type);
         }
->>>>>>> a7be1f5 (update)
     }
 }
