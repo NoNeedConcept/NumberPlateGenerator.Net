b78e7c673cd13762bcb33b42ae351d46d917c10a
Jan <jan.schloots@bluewin.ch>
2023-09-29 15:30:42 +0200

    add SettingsBuilder concept and many more stuff

--- src/NumberPlateGenerator.Net.Core/Settings/ISettings.cs
+++ src/NumberPlateGenerator.Net.Core/Settings/ISettings.cs
@@ -4,12 +4,4 @@ namespace NumberPlateGenerator.Net.Core.Settings
 {
     public interface ISettings
     { }
-
-    public abstract class SettingsBase<TProvinceEnum> : ISettings where TProvinceEnum : Enum
-    {
-        protected SettingsBase()
-        { }
-
-        public virtual TProvinceEnum Province { get; protected set; }
-    }
 }
