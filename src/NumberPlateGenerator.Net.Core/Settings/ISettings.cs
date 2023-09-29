608bc1b5b4a289523265782b78da5f7ade1ae572
Jan <jan.schloots@bluewin.ch>
2023-09-29 19:05:12 +0200

    refactor Settings

--- src/NumberPlateGenerator.Net.Core/Settings/ISettings.cs
+++ src/NumberPlateGenerator.Net.Core/Settings/ISettings.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using NumberPlateGenerator.Net.Core.CustomTypes;
 
 namespace NumberPlateGenerator.Net.Core.Settings
 {
     public interface ISettings
-    { }
+    {
+        DateRange Range { get; }
+    }
 }
